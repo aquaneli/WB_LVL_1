b027eade738e3ca4b1d23fa015704ee42ac4bbd3
Aqua Nelida <aquaneli@et-a6.kzn.21-school.ru>
2024-05-30 21:15:42 +0300

    ex1

--- EX_1/main.go
+++ EX_1/main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-/* Создали структру Human*/
+/* Создал структру Human*/
 type Human struct {
 	Age  int
 	Name string
 }
 
-/* Определили метод для структуры Human */
+/* Определил метод для структуры Human */
 func (Human) Speak() {
 	fmt.Println("Hello")
 }
 
-/* Определили метод для структуры Human , но используя получатель (recipient) мы можем изменить значение поля структуры при вызове метода*/
+/* Определил метод для структуры Human , но используя получатель (recipient) можно изменить значение поля структуры при вызове метода*/
 func (man *Human) NewAge(yers int) {
 	man.Age += yers
 }
 
-/* Здесь мы встроили поля из структуры Human, в том числе и методы */
+/* Здесь встроил поля из структуры Human, в том числе и методы */
 type Action struct {
 	Human
 }
@@ -32,7 +32,7 @@ func main() {
 	}
 	man.Speak()
 
-	/* После вызова этого метода значение Age изменится*/
+	/* После вызова этого метода значение Age изменится */
 	man.NewAge(10)
 	fmt.Println(man.Age, man.Name)
 }
