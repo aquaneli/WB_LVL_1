b23647e1618c032cbf7f8fdcd19e2a0be5048a44
Aqua Nelida <aquaneli@ge-b5.kzn.21-school.ru>
2024-06-02 16:43:19 +0300

    nonbuffer chan

--- EX_2/main.go
+++ EX_2/main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"fmt"
 	"sync"
+	// "time"
 )
 
 /* Вычисление квадратов чисел из массива используя конкурентность */
-
 func main() {
 	var arr = [...]int{2, 4, 6, 8, 10}
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+	ExampleOne(arr)
+	ExampleTwo(arr)
+	ExampleThree(arr)
 
-	ExampleOne(arr, &wg)
-
-	wg.Wait()
 }
 
 /* 1. Простой вариант вывода квадрата чисел */
-
-func ExampleOne(arr [5]int, wg *sync.WaitGroup) {
+func ExampleOne(arr [5]int) {
 	wgOne := sync.WaitGroup{}
 	wgOne.Add(5)
 	for i := range arr {
@@ -29,5 +26,40 @@ func ExampleOne(arr [5]int, wg *sync.WaitGroup) {
 		}(arr[i])
 	}
 	wgOne.Wait()
-	wg.Done()
+}
+
+/* 2. Вывода квадрата чисел с использованием небуфиризированного канала */
+func ExampleTwo(arr [5]int) {
+	wgTwo := sync.WaitGroup{}
+	wgTwo.Add(5)
+	ch := make(chan int)
+	defer close(ch)
+
+	for _, val := range arr {
+		go func(ch *chan int) {
+			fmt.Println(<-*ch)
+			wgTwo.Done()
+		}(&ch)
+		ch <- val * val
+	}
+	wgTwo.Wait()
+}
+
+/* 3. Вывода квадрата чисел с использованием небуфиризированного канала */
+func ExampleThree(arr [5]int) {
+	ch := make(chan int)
+	defer close(ch)
+
+	/* Кладем значение в канал первая горутина ставится на стоп пока значение
+	не считается из канала и все отсльные горутины на стопе пока канал не освободится */
+	for _, val := range arr {
+		go func(ch *chan int, val int) {
+			*ch <- val * val
+		}(&ch, val)
+	}
+
+	for range [5]int{} {
+		fmt.Println(<-ch)
+	}
+
 }
