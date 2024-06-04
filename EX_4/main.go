ad4bcd1d590132fe47be08887d112e64553865c1
Aqua Nelida <aquaneli@pr-d6.kzn.21-school.ru>
2024-06-04 20:56:08 +0300

    ex4

--- EX_4/main.go
+++ EX_4/main.go
@@ -8,34 +8,60 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithCancel()
+	ctx, cancel := context.WithCancel(context.Background())
+
 	workers := 0
+	fmt.Println("Select the number of workers")
 	fmt.Scan(&workers)
 	job := make(chan int, workers)
+	defer close(job)
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt)
+	defer close(sigChan)
 
-	defer close(job)
+	go func() {
+		if _, ok := <-sigChan; ok {
+			cancel()
+		}
+	}()
 
 	for i := 0; i < workers; i++ {
-		go func(job chan int) {
+		go func(ctx context.Context, job chan int) {
 		loop:
 			for {
 				select {
-				case <-sigChan:
-					close(sigChan)
+				case <-ctx.Done():
 					break loop
-				default:
-					fmt.Println(<-job)
+				case res := <-job:
+					fmt.Println(res)
 				}
-
 			}
-		}(job)
+		}(ctx, job)
 	}
 
+loop:
+	for i := 0; ; i++ {
+		select {
+		case <-ctx.Done():
+			break loop
+		default:
+			job <- i
+		}
+	}
+
+	fmt.Println("The program has stopped")
+
+}
+
+func worker(ctx context.Context, job chan int) {
+loop:
 	for {
-		job <- 1
+		select {
+		case <-ctx.Done():
+			break loop
+		case res := <-job:
+			fmt.Println(res)
+		}
 	}
-
 }
