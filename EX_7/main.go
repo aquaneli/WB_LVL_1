9088dd0e82d2ef486f386be12215d132adcd7172
Aqua Nelida <aquaneli@pr-d6.kzn.21-school.ru>
2024-06-06 11:46:44 +0300

    ex7

--- EX_7/main.go
+++ EX_7/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	// ExampleOne()
+	ExampleOne()
+	fmt.Println("-----")
 	ExampleTwo()
+	fmt.Println("-----")
+	ExampleThree()
 }
 
+/*  В этом примере для конкурентных вычислений используется mutex и группа ожидания */
 func ExampleOne() {
 	wg := sync.WaitGroup{}
 	wg.Add(10)
@@ -23,20 +27,68 @@ func ExampleOne() {
 			mutex.Unlock()
 		}(i)
 	}
-	fmt.Println(data)
+	wg.Wait()
+	for _, val := range data {
+		fmt.Println(val)
+	}
 }
 
+/*  В этом примере для конкурентных вычислений используется RWMutex и группа ожидания */
 func ExampleTwo() {
-	w := sync.Map{}
 	wg := sync.WaitGroup{}
+	rwmutex := sync.RWMutex{}
 	data := make(map[int]int)
+	wg.Add(10)
 	for i := 0; i < 10; i++ {
-		wg.Add(1)
 		go func(i int) {
-			w.Store(i, i*i)
+			rwmutex.Lock()
+			data[i] = i
 			wg.Done()
+			rwmutex.Unlock()
 		}(i)
 	}
 	wg.Wait()
-	fmt.Println(data)
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		rwmutex.RLock()
+		fmt.Println(data[i])
+		rwmutex.RUnlock()
+		wg.Done()
+	}
+
+	wg.Wait()
+}
+
+/*
+В этом примере для конкурентных вычислений используется sync.Map и группа ожидания
+sync.Map используется в основном для того чтобы избежать cache contetion
+*/
+func ExampleThree() {
+	m := sync.Map{}
+	wg := sync.WaitGroup{}
+
+	func() {
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func(i int) {
+				m.Store(i, i)
+				wg.Done()
+			}(i)
+		}
+	}()
+
+	wg.Wait()
+	func() {
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func(i int) {
+				v, _ := m.Load(i)
+				fmt.Println(v)
+				wg.Done()
+			}(i)
+		}
+	}()
+
+	wg.Wait()
 }
